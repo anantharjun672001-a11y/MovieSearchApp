cc914fd69b920797a782298866fd28dece3cb0e7
anantharjun672001-a11y <anantharjun672001@gmail.com>
2026-01-29 14:22:14 +0530

    MovieCard setted

--- src/components/MovieCard.jsx
+++ src/components/MovieCard.jsx
@@ -1,9 +1,16 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 
-const MovieCard = () => {
+const MovieCard = ({movie}) => {
     return (
         <div>
-            
+            <img src={movie.Poster} alt={movie.Title}/>
+            <h3>{movie.Title}</h3>
+            <p>{movie.Year}</p>
+
+            <Link to={`/movie/movie.imdbID`}>
+                View Details
+            </Link>
         </div>
     );
 };
